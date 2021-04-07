ee1abb770336ddb1c355273b4f5da28120bbf174
Alekzanderor <alekzanderor@gmail.com>
2021-04-08 01:27:17 +0300

    Задание 2+1

--- Task2/src/main/java/Base.java
+++ Task2/src/main/java/Base.java
@@ -8,16 +8,23 @@ public class Base {
 
         InputStreamReader r=new InputStreamReader(System.in);
         BufferedReader br=new BufferedReader(r);
+
+        //считываем пользовательский ввод
         System.out.println("Enter first number");
         Float firstN= Float.valueOf(br.readLine());
         System.out.println("Enter second number");
         Float secondN= Float.valueOf(br.readLine());
-        System.out.println("Choose operand type");
-        String operand = br.readLine();
-        if (operand.equals("+")) { System.out.println(firstN+secondN) ;}
-            else if (operand.equals("-")) {System.out.println(firstN-secondN);}
-            else if (operand.equals("*")) {System.out.println(firstN*secondN);}
-            else if (operand.equals("/")) {System.out.println(firstN / secondN);}
+        System.out.println("Choose operator");
+        String operator = br.readLine();
+
+        /**
+         * проверяя операнд с помощью if else производим операцию с введенным значениями
+         * @author Орехов Александр
+         */
+        if (operator.equals("+")) { System.out.printf("%.4f", firstN+secondN) ;}
+            else if (operator.equals("-")) {System.out.println(firstN-secondN);}
+            else if (operator.equals("*")) {System.out.println(firstN*secondN);}
+            else if (operator.equals("/")) {System.out.println(firstN / secondN);}
             else { System.out.println("Incorrect operand" + "operand"); }
     }
 }
